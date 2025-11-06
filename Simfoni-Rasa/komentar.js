ee347bb13d74a0cbbf61610207613e40cc73d831
syfiann <syamsarfian@gmail.com>
2025-11-06 07:10:15 +0700

    Delete Simfoni-Rasa/komentar.js

--- Simfoni-Rasa/komentar.js
+++
@@ -1,9 +0,0 @@
-const addComment = async (recipeId, comment) => {
-  const { data: { user } } = await supabase.auth.getUser();
-
-  const { data, error } = await supabase.from("comments").insert({
-    recipe_id: recipeId,
-    user_id: user.id,
-    comment: comment,
-  });
-};
